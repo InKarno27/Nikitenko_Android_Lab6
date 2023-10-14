fda769eb780a144ff2cd53498686d4e3fee6338f
Student <Student@IIST.local>
2023-10-14 10:25:36 +0300

    Initial commit

---
+++ app/src/main/java/com/example/nikitenko_android_lab6/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.nikitenko_android_lab6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText firstNameText;
+    EditText secondNameText;
+    EditText groupText;
+    EditText ageText;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        firstNameText = findViewById(R.id.editTextTextPersonName);
+        secondNameText = findViewById(R.id.editTextTextPersonName2);
+        groupText = findViewById(R.id.editTextNumber);
+        ageText = findViewById(R.id.editTextNumber2);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String firstName = firstNameText.getText().toString();
+                String secondName = secondNameText.getText().toString();
+                String group = groupText.getText().toString();
+                int age = Integer.parseInt(ageText.getText().toString());
+                Student student1 = new Student(firstName, secondName, age,
+                        group);
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra(Student.class.getSimpleName(), student1);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/nikitenko_android_lab6/SecondActivity.java
@@ -0,0 +1,29 @@
+package com.example.nikitenko_android_lab6;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_second);
+        TextView textView = new TextView(this);
+        textView.setTextSize(26);
+        textView.setPadding(16, 16, 16, 16);
+        Bundle arguments = getIntent().getExtras();
+        Student student1;
+        if (arguments != null) {
+            student1 = (Student)
+                    arguments.getSerializable(Student.class.getSimpleName());
+            textView.setText("Имя: " + student1.getFirstName() + " "
+                    + student1.getLastName()
+                    + "\nГруппа: "
+                    + student1.getGroup() +
+                    "\nВозраст: " + String.valueOf(student1.getAge()));
+        }
+        setContentView(textView);
+    }
+}
+
